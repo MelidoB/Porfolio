a1b522725e2ddbbd393b12a8d407e1a48346f478
melido <enrriquebello10@gmail.com>
2025-02-16 16:40:03 -0500

    including pictures

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MeImage from '../assets/Home/Me.jpg';
 
 function HeroSection() {
   return (
@@ -8,10 +9,10 @@ function HeroSection() {
                         md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
             Before they sold out
-            <br className="hidden lg:inline-block" />readymade gluten
+            <br className="hidden lg:inline-block" /> readymade gluten
           </h1>
           <p className="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. 
+            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag.
           </p>
           <div className="flex justify-center">
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
@@ -23,7 +24,7 @@ function HeroSection() {
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+          <img className="object-cover object-center rounded" alt="hero" src={MeImage} />
         </div>
       </div>
     </section>
